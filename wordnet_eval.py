ea1d180c2bdd63be88153a8f6959547ecf8d643e
John Gamboa <vaulttech@gmail.com>
2017-08-06 18:04:59 +0200

    Some functions for exploratory analysis of Wordnet
    Apparently, the WordNet is much more restricted than I expected. From
    its 117k synsets, around 63k are composed by only one word, and only
    around 7k lemmas have antonyms.
    
     Changes to be committed:
    	modified:   wordnet_eval.py

--- wordnet_eval.py
+++ wordnet_eval.py
@@ -24,6 +24,7 @@ def get_triples():
 def get_all_words(triples):
     all_words = []
     for i in triples:
+        # Yes... we assume the triples are always correct
         all_words.append(i[0])
         all_words.append(i[1])
         all_words.append(i[2])
@@ -31,4 +32,55 @@ def get_all_words(triples):
     deduplicated_words = set(all_words)
     return list(deduplicated_words)
 
+def get_singleton_synsets():
+    return [s for s in wn.all_synsets() if len(s.lemmas()) == 1 ]
+    #ret = []
+    #for s in wn.all_synsets():
+    #    if len(s.lemmas()) == 1:
+    #        ret.append(s)
+    #return ret
 
+def get_antonyms(remove_duplicates=False):
+    antonyms = []
+    lemmas_with_antonyms = []
+    lemma_antonym_pairs = []
+    for l in wn.all_lemma_names():
+        synsets = wn.synsets(l)
+        for s in synsets:
+            for s_l in s.lemmas():
+                if s_l.name() != l:
+                    # Here I only care about the antonyms of the current lemma
+                    continue
+
+                found_antonym = False
+                for a in s_l.antonyms():
+                    antonyms.append(a)
+                    found_antonym = True
+
+                    if remove_duplicates:
+                        lemma_antonym_pairs.append(frozenset((s_l, a)))
+                    else:
+                        lemma_antonym_pairs.append((s_l, a))
+
+
+                if found_antonym:
+                    lemmas_with_antonyms.append(s_l)
+
+    if remove_duplicates:
+        lemma_antonym_pairs = list(set(lemma_antonym_pairs))
+
+    return antonyms, lemmas_with_antonyms, lemma_antonym_pairs
+
+#import spacy
+#import numpy as np
+#nlp = None
+# def initialize_word_embeddings(words):
+#     if nlp is None:
+#         nlp = spacy.load('en')
+#
+#     embeddings = np.zeros([len(words), vec_size])
+#     for i in words:
+#
+#         embeddings[i] = word_vec
+#
+#     return embeddings
